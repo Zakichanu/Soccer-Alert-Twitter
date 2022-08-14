4f1e08e3f4888bb9225a28f5382b50bb5d6dd0e7
Zakaria Tobbal <tzakaria@hotmail.fr>
2022-08-14 11:31:20 +0200

    adding condition when there is no fixture for the day in fixtureResult

--- src/FixtureResult.ts
+++ src/FixtureResult.ts
@@ -4,8 +4,8 @@ import functions from '../helper/functions';
 
 // Function to retrieve the match of the day
 const fixturePreview = () => {
-    cron.schedule('0 10 * * *', async () => {
-        console.log("It's 10 o'clock !!! Match preview of the day...");
+    cron.schedule('0 12 * * *', async () => {
+        console.log("It's 12 o'clock !!! Match preview of the day...");
         callResult("⏰ Match of the day | ");
 
     })
@@ -21,64 +21,82 @@ const fixtureResult = () => {
 
 
 const callResult = async (type: string) => {
-    console.log("Premier League...")
     let premierLeagueFixtures: Array<fixtureResponse> = await functions.getMatchResult(constants.premierLeague.id);
     constants.premierLeague.fixtureOfTheDay = premierLeagueFixtures;
-    constants.listOfTweetsThread.push(type + "#PremierLeague")
-    await fillArrayForThreadTweet(constants.premierLeague.fixtureOfTheDay);
+    if (constants.premierLeague.fixtureOfTheDay.length > 0) {
+        console.log("Premier League...")
+        constants.listOfTweetsThread.push(type + "#PremierLeague")
+        await fillArrayForThreadTweet(constants.premierLeague.fixtureOfTheDay);
+    }
+
 
     // Waiting 2 minutes to avoid any conflict
     await functions.wait(120000);
 
-    console.log("Ligue 1...")
     let ligue1Fixtures: Array<fixtureResponse> = await functions.getMatchResult(constants.ligue1.id);
     constants.ligue1.fixtureOfTheDay = ligue1Fixtures;
-    constants.listOfTweetsThread.push(type + "#Ligue1")
-    await fillArrayForThreadTweet(constants.ligue1.fixtureOfTheDay);
+    if (constants.ligue1.fixtureOfTheDay.length > 0) {
+        console.log("Ligue 1...")
+        constants.listOfTweetsThread.push(type + "#Ligue1")
+        await fillArrayForThreadTweet(constants.ligue1.fixtureOfTheDay);
+    }
 
     // Waiting 2 minutes to avoid any conflict
     await functions.wait(120000);
 
-    console.log("Bundesliga...")
-    let bundesligaFixtures : Array<fixtureResponse> = await functions.getMatchResult(constants.bundesliga.id);
+    let bundesligaFixtures: Array<fixtureResponse> = await functions.getMatchResult(constants.bundesliga.id);
     constants.bundesliga.fixtureOfTheDay = bundesligaFixtures;
-    constants.listOfTweetsThread.push(type + "#Bundesliga")
-    await fillArrayForThreadTweet(constants.bundesliga.fixtureOfTheDay);
+    if (constants.bundesliga.fixtureOfTheDay.length > 0) {
+        console.log("Bundesliga...")
+        constants.listOfTweetsThread.push(type + "#Bundesliga")
+        await fillArrayForThreadTweet(constants.bundesliga.fixtureOfTheDay);
+    }
 
     // Waiting 2 minutes to avoid any conflict
     await functions.wait(120000);
 
-    console.log("LaLiga...")
-    let laLigaFixtures : Array<fixtureResponse> = await functions.getMatchResult(constants.laLiga.id);
+    let laLigaFixtures: Array<fixtureResponse> = await functions.getMatchResult(constants.laLiga.id);
     constants.laLiga.fixtureOfTheDay = laLigaFixtures;
-    constants.listOfTweetsThread.push(type + "#LaLiga")
-    await fillArrayForThreadTweet(constants.laLiga.fixtureOfTheDay);
+    if (constants.laLiga.fixtureOfTheDay.length > 0) {
+        console.log("LaLiga...")
+        constants.listOfTweetsThread.push(type + "#LaLiga")
+        await fillArrayForThreadTweet(constants.laLiga.fixtureOfTheDay);
+    }
 
     // Waiting 2 minutes to avoid any conflict
     await functions.wait(120000);
 
-    console.log("UCL...")
-    let uclFixtures : Array<fixtureResponse> = await functions.getMatchResult(constants.ucl.id);
+    let uclFixtures: Array<fixtureResponse> = await functions.getMatchResult(constants.ucl.id);
     constants.ucl.fixtureOfTheDay = uclFixtures;
-    constants.listOfTweetsThread.push(type + "#UCL")
-    await fillArrayForThreadTweet(constants.ucl.fixtureOfTheDay);
+    if (constants.ucl.fixtureOfTheDay.length > 0) {
+        console.log("UCL...")
+        constants.listOfTweetsThread.push(type + "#UCL")
+        await fillArrayForThreadTweet(constants.ucl.fixtureOfTheDay);
+    }
 
     // Waiting 2 minutes to avoid any conflict
     await functions.wait(120000);
 
-    console.log("Europa League...")
-    let europaLeagueFixtures : Array<fixtureResponse> = await functions.getMatchResult(constants.europaLeague.id);
+
+    let europaLeagueFixtures: Array<fixtureResponse> = await functions.getMatchResult(constants.europaLeague.id);
     constants.europaLeague.fixtureOfTheDay = europaLeagueFixtures;
-    constants.listOfTweetsThread.push(type + "#EL")
-    await fillArrayForThreadTweet(constants.europaLeague.fixtureOfTheDay);
+    if (constants.europaLeague.fixtureOfTheDay.length > 0) {
+        console.log("Europa League...")
+        constants.listOfTweetsThread.push(type + "#EL")
+        await fillArrayForThreadTweet(constants.europaLeague.fixtureOfTheDay);
+    }
+
 
     // Waiting 2 minutes to avoid any conflict
     await functions.wait(120000);
 
-    console.log("Serie A...")
-    let serieAFixtures : Array<fixtureResponse> = await functions.getMatchResult(constants.serieA.id);
+    let serieAFixtures: Array<fixtureResponse> = await functions.getMatchResult(constants.serieA.id);
     constants.serieA.fixtureOfTheDay = serieAFixtures;
-    constants.listOfTweetsThread.push(type + "#SerieA")
+    if (constants.serieA.fixtureOfTheDay.length > 0) {
+        console.log("Serie A...")
+        constants.listOfTweetsThread.push(type + "#SerieA")
+        await fillArrayForThreadTweet(constants.serieA.fixtureOfTheDay);
+    }
 
     console.log("Done !");
 }
@@ -88,7 +106,7 @@ const fillArrayForThreadTweet = async (fixtureResult: Array<fixtureResponse>) =>
         const homeGoal: number = iterator.goals.home === null ? 0 : iterator.goals.home;
         const awayGoal: number = iterator.goals.away === null ? 0 : iterator.goals.away;
 
-        const result: string = iterator.teams.home.name + " " + homeGoal +  " - " + awayGoal + " " + iterator.teams.away.name + "\n🕚" + new Date(iterator.fixture.date).getHours() + ":" + (new Date(iterator.fixture.date).getMinutes() < 10 ? '0' : '') + new Date(iterator.fixture.date).getMinutes();
+        const result: string = iterator.teams.home.name + " " + homeGoal + " - " + awayGoal + " " + iterator.teams.away.name + "\n🕚" + new Date(iterator.fixture.date).getHours() + ":" + (new Date(iterator.fixture.date).getMinutes() < 10 ? '0' : '') + new Date(iterator.fixture.date).getMinutes();
         constants.listOfTweetsThread.push(result);
         console.log(result);
     }
